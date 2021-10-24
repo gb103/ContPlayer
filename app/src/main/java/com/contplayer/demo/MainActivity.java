edf732a18fec13d7f77f010f91bd89a931e8beff
Gaurav Bansal <gaurav.bansal1@gaana.com>
2021-10-24 11:20:28 +0530

    - added sample HLS urls

--- app/src/main/java/com/contplayer/demo/MainActivity.java
+++ app/src/main/java/com/contplayer/demo/MainActivity.java
@@ -32,9 +32,11 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<String> streamUrlList = new ArrayList<>();
         streamUrlList.add("https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8");
         streamUrlList.add("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8");
-        streamUrlList.add("https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8");
+        streamUrlList.add("https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8");
         streamUrlList.add("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4");
         streamUrlList.add("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4");
+        streamUrlList.add("https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8");
+        streamUrlList.add("https://moctobpltc-i.akamaihd.net/hls/live/571329/eight/playlist.m3u8");
         demoPlayerQueue.setArrayList(streamUrlList);
         return demoPlayerQueue;
     }
